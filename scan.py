7a6c57112e9620d6c5283ea0c8f5216a8ef6feae
Ankit Solanki <ankit03june@gmail.com>
2018-05-05 17:40:06 +0530

    ADD: function for summarising text completed

--- scan.py
+++ scan.py
@@ -1,4 +1,68 @@
+import nltk
 import argparse
+import nltk.corpus
+import operator
+from string import punctuation
+from collections import defaultdict
+
+
+def frequency(textTokens, rejectExtremeValues=True):
+	
+	freqDict = defaultdict(float)
+	
+	for line in textTokens:
+		for w in line:
+			if w not in set(nltk.corpus.stopwords.words('english')+list(punctuation)):
+				freqDict[w]+=1
+
+	if rejectExtremeValues:
+		maximum = float(max(freqDict.values()))
+		
+		for (w, v) in freqDict.items():
+			freqDict[w] = v / maximum
+			if freqDict[w] >= 0.85 or freqDict[w] <= 0.15:
+				freqDict[w] = v - 1 
+
+	return freqDict
+
+
+def calculateProb(fDict, textTokens, num, ):
+	
+	sentenceRank = defaultdict(float)
+	
+	for sentence in textTokens:
+		s = " ".join(sentence)
+		for word in sentence:
+			if word in fDict:
+				sentenceRank[s] += fDict[word]
+
+	summaryLines=[]
+
+	for i in range(0, num):
+		if len(sentenceRank)!=0:
+			s = max( sentenceRank.items(), key = operator.itemgetter(1))[0]
+			
+			summaryLines.append(s)
+			del sentenceRank[s]
+
+	return summaryLines
+
+
+def summary(text, num):
+	"""takes input as text and n in which summary of text
+	 should be represented"""
+	
+	sentenceTokens = nltk.sent_tokenize(text)
+	
+	textTokens=[]
+	for tok in sentenceTokens:
+		textTokens.append(nltk.word_tokenize(tok.lower())) 
+
+	fDict = frequency(textTokens, False)
+
+	summary = calculateProb(fDict, textTokens, num)
+
+	return summary	
 
 
 if __name__=="__main__":
@@ -17,3 +81,6 @@ if __name__=="__main__":
 		text = f.read()
 
 		text = text.replace('\n', "")
+		summ = summary(text, args.number)
+
+		print(summ)
